9707d0d88e4cc87721690f82572e1376c410546f
jonas_vera <jonasverasilva@gmail.com>
2021-07-21 21:34:30 -0300

    Implementação do metodo select

--- src/Api.Data/Repository/BaseRepository.cs
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -54,16 +54,21 @@ namespace Api.Data.Repository
             return item;
         }
 
-        public Task<T> SelectAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
+         public async Task<T> SelectAsync (Guid id) {
+            try {
+                return await _dataset.SingleOrDefaultAsync (p => p.Id.Equals (id));
+            } catch (Exception ex) {
 
-        public Task<IEnumerable<T>> SelectAsync()
-        {
-            throw new NotImplementedException();
+                throw ex;
+            }
+        } 
+        public async Task<IEnumerable<T>> SelectAsync () {
+            try {
+                return await _dataset.ToListAsync ();
+            } catch (Exception ex) {
+                throw ex;
+            }
         }
-
         public async Task<T> UpdateAsync(T item)
         {
            try{
